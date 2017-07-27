a85c7288b7e00ecb4c4129e5467fffd6435d9d7f
Sylver Yagi <silvery4543@gmail.com>
2017-07-28 00:29:36 +1000

    the app now directly dials the transfer

--- app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
+++ app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
@@ -1,12 +1,18 @@
 package com.sudotechpng.android.evdreselleraid;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,34 +20,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        QuickContactBadge qcbOnTap = (QuickContactBadge)findViewById(R.id.search_contact);
-//        qcbOnTap.setOnClickListener(onQCBTouch);
-
-        Button btn_dial = (Button)findViewById(R.id.btn_dialler);
-        btn_dial.setOnClickListener(onTouchEvent);
+        Button btn_dial = (Button) findViewById(R.id.btn_dialler);
+        btn_dial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText number = (EditText) findViewById(R.id.phone_number);
+                EditText amount = (EditText) findViewById(R.id.amount);
+                EditText resellerPin = (EditText) findViewById(R.id.reseller_pin);
+                String getPhoneNumber = String.valueOf(number.getText());
+                String getAmount = String.valueOf(amount.getText());
+                String getResellerPin = String.valueOf(resellerPin.getText());
+                String ussd_code = String.format("*889*2*%s*%s*%s#", getPhoneNumber, getAmount, getResellerPin);
+                try {
+                    ussd_code = URLEncoder.encode(ussd_code, "UTF-8");
+                    Intent dial = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + ussd_code));
+                    if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    startActivity(dial);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
-
-//    private View.OnClickListener onQCBTouch=new View.OnClickListener(){
-//        @Override
-//        public void onClick(View view) {
-//            Intent lookupContact = new Intent(Intent.ACTION_VIEW, ContactsContract.Contacts.CONTENT_LOOKUP_URI);
-//            startActivity(lookupContact);
-//        }
-//    };
-    private View.OnClickListener onTouchEvent=new View.OnClickListener(){
-        @Override
-        public void onClick(View view) {
-//        variables
-            EditText number = (EditText)findViewById(R.id.phone_number);
-            EditText amount = (EditText)findViewById(R.id.amount);
-            EditText resellerPin = (EditText)findViewById(R.id.reseller_pin);
-            String getPhoneNumber = String.valueOf(number.getText());
-            String getAmount = String.valueOf(amount.getText());
-            String getResellerPin = String.valueOf(resellerPin.getText());
-            String ussd_code = String.format("*889*2*%s*%s*%s*1", getPhoneNumber, getAmount, getResellerPin);
-            Intent dial = new Intent(Intent.ACTION_VIEW,Uri.parse("tel:"+ussd_code+"#"));
-            startActivity(dial);
-        };
-    };
 }
