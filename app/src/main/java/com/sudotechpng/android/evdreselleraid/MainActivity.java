5087b7d4507a23bb684e7453bef88f1e297b380c
Sylver Yagi <silvery4543@gmail.com>
2017-08-24 15:44:22 +1000

    did a fair amount of changes to the codes: * added a user settings layout and class * added ad banners * changes to the main layout * app logic still remained the same

--- app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
+++ app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sudotechpng.android.evdreselleraid;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -10,16 +11,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String mAdMobAppId = "ca-app-pub-3511968554265090~9944410382";
+    private String main_bannerAdUnitId = "ca-app-pub-3511968554265090/7130367173";
+    private String main_user_interstitialAdUnitId = "ca-app-pub-3511968554265090/7113538385";
+    private String user_bannerAdUnitId = "ca-app-pub-3511968554265090/9855251791";
+    private InterstitialAd muiAd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // admob code
+        MobileAds.initialize(this,mAdMobAppId);
+        // main activity banner ad
+        AdView mAdView = (AdView)findViewById(R.id.main_adBanner);
+        AdRequest requestMainBanner = new AdRequest.Builder().build();
+        mAdView.loadAd(requestMainBanner);
+        // main-user interstitial ad
+        muiAd = new InterstitialAd(this);
+        muiAd.setAdUnitId(main_user_interstitialAdUnitId);
+        AdRequest requestInterstitial = new AdRequest.Builder().build();
+        muiAd.loadAd(requestInterstitial);
+
+        // send topap code
         Button btn_dial = (Button) findViewById(R.id.btn_dialler);
         btn_dial.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,13 +59,6 @@ public class MainActivity extends AppCompatActivity {
                     ussd_code = URLEncoder.encode(ussd_code, "UTF-8");
                     Intent dial = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + ussd_code));
                     if (ActivityCompat.checkSelfPermission(view.getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                        // TODO: Consider calling
-                        //    ActivityCompat#requestPermissions
-                        // here to request the missing permissions, and then overriding
-                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                        //                                          int[] grantResults)
-                        // to handle the case where the user grants the permission. See the documentation
-                        // for ActivityCompat#requestPermissions for more details.
                         return;
                     }
                     startActivity(dial);
@@ -50,5 +67,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void sendMessage(View v){
+        // do something in response to button click
+        Intent intent = new Intent(this, UserSettingsActivity.class);
+        if(muiAd.isLoaded()){
+            muiAd.show();
+        }
+        startActivity(intent);
     }
 }
