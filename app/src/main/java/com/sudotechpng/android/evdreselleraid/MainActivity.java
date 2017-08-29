dddc172943ed855c7c7777790af1699f8b0756eb
Sylver Yagi <silvery4543@gmail.com>
2017-08-29 15:06:59 +1000

    1.2.5 update applied to the following: * introduced EVD Reseller PIN save feature * removed pin text input from Main as a result * tested and ensured its working with no crashes

--- app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
+++ app/src/main/java/com/sudotechpng/android/evdreselleraid/MainActivity.java
@@ -1,16 +1,27 @@
 package com.sudotechpng.android.evdreselleraid;
 
 import android.Manifest;
-import android.app.DownloadManager;
+import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
@@ -20,11 +31,8 @@ import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String mAdMobAppId = "ca-app-pub-3511968554265090~9944410382";
-    private String main_bannerAdUnitId = "ca-app-pub-3511968554265090/7130367173";
-    private String main_user_interstitialAdUnitId = "ca-app-pub-3511968554265090/7113538385";
-    private String user_bannerAdUnitId = "ca-app-pub-3511968554265090/9855251791";
     private InterstitialAd muiAd;
+    private SharedPreferences msp_pin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +40,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // admob code
-        MobileAds.initialize(this,mAdMobAppId);
+        String mAdMobAppId = "ca-app-pub-3511968554265090~9944410382";
+        MobileAds.initialize(this, mAdMobAppId);
         // main activity banner ad
         AdView mAdView = (AdView)findViewById(R.id.main_adBanner);
         AdRequest requestMainBanner = new AdRequest.Builder().build();
         mAdView.loadAd(requestMainBanner);
         // main-user interstitial ad
         muiAd = new InterstitialAd(this);
+        String main_user_interstitialAdUnitId = "ca-app-pub-3511968554265090/7113538385";
         muiAd.setAdUnitId(main_user_interstitialAdUnitId);
         AdRequest requestInterstitial = new AdRequest.Builder().build();
         muiAd.loadAd(requestInterstitial);
@@ -50,10 +60,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 EditText number = (EditText) findViewById(R.id.phone_number);
                 EditText amount = (EditText) findViewById(R.id.amount);
-                EditText resellerPin = (EditText) findViewById(R.id.reseller_pin);
                 String getPhoneNumber = String.valueOf(number.getText());
                 String getAmount = String.valueOf(amount.getText());
-                String getResellerPin = String.valueOf(resellerPin.getText());
+                String getResellerPin = String.valueOf(msp_pin.getInt("evdpin",8464));
                 String ussd_code = String.format("*889*2*%s*%s*%s#", getPhoneNumber, getAmount, getResellerPin);
                 try {
                     ussd_code = URLEncoder.encode(ussd_code, "UTF-8");
@@ -69,12 +78,28 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void sendMessage(View v){
+    public void gotoUserSettings(View v){
         // do something in response to button click
         Intent intent = new Intent(this, UserSettingsActivity.class);
         if(muiAd.isLoaded()){
+            Toast.makeText(this,"Interstitial loaded!",Toast.LENGTH_LONG).show();
             muiAd.show();
         }
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        msp_pin = getSharedPreferences("ResellerPIN",MODE_PRIVATE);
+        int tmp_pin = msp_pin.getInt("evdpin",8464);
+
+        if (tmp_pin == 8464){
+            startActivity(new Intent(this,UserSettingsActivity.class));
+        } else {
+//            TextView rPIN = (TextView) findViewById(R.id.reseller_pin_label);
+//            rPIN.setText("<"+tmp_pin+">");
+            Toast.makeText(this,"PIN "+tmp_pin+" accepted!",Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/sudotechpng/android/evdreselleraid/UserSettingsActivity.java
+++ app/src/main/java/com/sudotechpng/android/evdreselleraid/UserSettingsActivity.java
@@ -1,13 +1,59 @@
 package com.sudotechpng.android.evdreselleraid;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 public class UserSettingsActivity extends AppCompatActivity {
+
+    private SharedPreferences sharedPref;
+    private EditText EVDPIN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        // admob code
+        String mAdMobAppId = "ca-app-pub-3511968554265090~9944410382";
+        MobileAds.initialize(this, mAdMobAppId);
+        // main activity banner ad
+        AdView mAdView = (AdView)findViewById(R.id.user_adBanner);
+        AdRequest requestUserBanner = new AdRequest.Builder().build();
+        mAdView.loadAd(requestUserBanner);
+
+        Button saveUser = (Button)findViewById(R.id.saveUserBtn);
+        saveUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserSettings();
+            }
+        });
+    }
+
+    private void saveUserSettings(){
+        EVDPIN = (EditText)findViewById(R.id.user_evdPIN);
+        String storepin = String.valueOf(EVDPIN.getText().toString());
+        Toast.makeText(this,"PIN "+storepin+" is saved!",Toast.LENGTH_LONG).show();
+
+        SharedPreferences mSharedPref = getSharedPreferences("ResellerPIN", MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSharedPref.edit();
+        mEditor.putInt("evdpin", Integer.parseInt(storepin));
+        mEditor.apply();
+    }
+
+    private int getResellerPIN(){
+        SharedPreferences mSharedPref = getSharedPreferences("ResellerPIN", MODE_PRIVATE);
+        return mSharedPref.getInt("evdpin", 8464);
     }
 }
